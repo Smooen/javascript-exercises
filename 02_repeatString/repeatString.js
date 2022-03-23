e175585247ab6f75bf5edce0da1853ecffb705b8
Smooen <smooenus@gmail.com>
2022-03-23 12:34:38 +0100

    First try needs fixing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let str = string;
+    for(let i = 1; i < number; i++){
+        str += string;
+    }
+    return str;
 };
 
 // Do not edit below this line
