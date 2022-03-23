b11cc4287fd10b2570ea1fa6c9f10231de743028
Smooen <smooenus@gmail.com>
2022-03-23 13:06:09 +0100

    Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    reverse = '';
+    let j = string.length
+    for(let i = 0; i < j; i++){
+       reverse += string.charAt(string.length - 1);
+       string = string.slice(0, -1);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
