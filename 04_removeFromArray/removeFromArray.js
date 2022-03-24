1931ab7f2fa331606c09dbd9512c85f78e901783
Smooen <smooenus@gmail.com>
2022-03-24 12:19:13 +0100

    Finish Exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
-const removeFromArray = function(array, number) {
-    for(let i = 0; i < array.length; i++){
-        if(array[i] === number){
-            array.splice(i, 1);
+const removeFromArray = function (array) {
+    for (let j = 1; j < arguments.length; j++) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === arguments[j]) {
+                array.splice(i, 1);
+            }
         }
     }
     return array;
