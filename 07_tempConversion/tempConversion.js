d42b2236f8f456f57f36a0ad53da5459435e2463
Smooen <smooenus@gmail.com>
2022-03-25 11:19:34 +0100

    Finish Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(deg) {
+  num = (deg - 32)*5/9;
+  return +num.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(deg) {
+  num = deg * 9/5 + 32;
+  return +num.toFixed(1);
 };
 
 // Do not edit below this line
